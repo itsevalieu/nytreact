3057b697d8ac11ce29d512290fcfa35ddc693b21
Eva Lieu <itsevalieu@gmail.com>
2017-04-13 14:52:37 -0700

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,31 +28,4 @@ module.exports = {
   // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
   // Without this the console says all errors are coming from just coming from bundle.js
   devtool: "eval-source-map"
-  devtool: 'source-map',
-    entry: [
-        './app/app.js'
-    ],
-    output: {
-        filename: 'public/bundle.js',
-    },
-    module: {
-        loaders: [
-            // js
-            {
-                test: /\.jsx?$/,
-                loaders: ['babel-loader'],
-                include: path.join(__dirname, 'src')
-            },
-            {
-                test: /\.css$/,
-                loaders: ["style-loader", "css-loader"],
-                include: path.join(__dirname, '/src/styles')
-                
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loaders: ["url-loader"]
-            }
-        ]
-    }
 };
